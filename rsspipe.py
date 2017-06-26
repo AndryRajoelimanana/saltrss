105d7958c690b0960f8251574ed4542c7d3163f1
AndryRajoelimanana <randryfitiavana@gmail.com>
2017-06-26 17:30:45 +0200

    Add files via upload

---
+++ rsspipe.py
@@ -0,0 +1,264 @@
+#! /usr/bin/env python									                             
+#########################################################################################
+#                                                                                       #							
+#   Dr. Andry Rajoelimanana                                                             #	
+#                                                                                       #
+#   SALT/RSS longslit reduction                                                         #
+#                                                                                       #		
+#########################################################################################
+
+import os
+from pyraf import iraf
+from astropy.io import fits
+
+import rsstools as rt
+import numpy as np
+
+iraf.noao(_doprint=0)
+iraf.twodspec(_doprint=0)
+iraf.longslit(_doprint=0)
+iraf.apextract(dispaxis='1')
+iraf.stsdas(_doprint=0)
+iraf.analysis(_doprint=0)
+iraf.fitting(_doprint=0)
+
+config = rt.load_config('rssconfig.yml')
+
+saltreddir = config['rss']['rssdir'] 
+caldir = config['rss']['caldir']
+extinct= config['rss']['extdir']
+
+# Wavelength Calibration of the ARC spectra       
+def rssidentify(arclist, config):
+    ref_spec= os.path.splitext(arclist)[0]
+    reference= rt.makesetups(arclist,"arc")
+    referfits = reference+'.fits'  
+    referid_db = saltreddir+"/database/id"+str(reference)
+    referfits_db = saltreddir+'/'+referfits   
+    coordlist= reference.split('_')[0].lower()+'.dat'       
+    coordfile = config['rss']['linelist_dir']+str(coordlist)
+
+    rt.loadparam(config, ['iraf.identify', 'iraf.reidentify', 'iraf.fitcoords'])
+      
+    if reference == 'NONE_NONE_NONE_NONE_NONE':
+        print "***** rssidentify error"
+        print "***** We can not get your spectral setup *****"
+        return
+    
+    if os.path.isfile(referfits_db) and os.path.isfile(referid_db):
+        rt.getsh('cp %s ./'  % (referfits_db))
+        if not os.path.exists('database'): os.system('mkdir database')
+        rt.getsh('cp %s database/'  % (referid_db))
+
+    else:
+        print referfits_db, referid_db
+        print "This setup is not in our database (setup = %s)" % (reference)
+        rt.getsh('cp %s ./%s'  % (arclist, referfits))
+        rt.rsswave(referfits)
+        if os.path.isfile(coordfile):
+            rt.getsh('cp %s ./' %  (coordfile))
+        else :
+            print "***** rssidentify error *** The coordinate list for this arc IS NOT in our directory *****"
+            return            
+        iraf.identify(reference, coordlist=coordlist, mode='h')
+        iraf.reidentify(reference, reference, coordlist=coordlist, mode='h')
+        iraf.fitcoord(reference, mode='h')
+
+        os.system('cp '+referfits+' '+saltreddir+'/'+referfits)
+        os.system('cp database/id'+reference+' '+saltreddir+'/database/id'+reference)
+        os.system('cp database/fc'+reference+' '+saltreddir+'/database/fc'+reference)
+
+
+    print "'***** Reducing of ARCs *****'"
+
+    arc_id = "database/id"+str(ref_spec)
+    arc_fc = "database/fc"+str(ref_spec)
+
+    if not os.path.isfile(arc_id) or not os.path.isfile(arc_fc):
+        iraf.reidentify(reference, ref_spec, coordlist=coordlist, mode='h')
+        iraf.reidentify(ref_spec, ref_spec, coordlist=coordlist, mode='h')        
+        iraf.fitcoord(ref_spec, mode='h')
+       
+# Wavelength calibration, background substraction, and flux calibration of the target spectra        
+def rssextractone(images, arcl, config, logfile, do_error=False):
+
+    print "'***** Reducing of objects ***** '"
+    infiles = os.path.splitext(images)[0]
+    ref_spec = os.path.splitext(arcl)[0]
+    if os.path.isfile('b'+images): os.system('mv b'+images+' '+images)
+    hduorig= fits.open(images)
+    hduimg = fits.PrimaryHDU(data=hduorig[0].data,header=hduorig[0].header)
+    os.system('mv  '+images+'  b'+images)
+    hduimg.writeto(images)
+    hduvarimg = fits.PrimaryHDU(data=hduorig[1].data,header=hduorig[0].header)
+    rt.rmexist([infiles+'_var.fits'])
+    hduvarimg.writeto(infiles+'_var.fits')  
+    rt.prepare_image(images, do_error=do_error)
+    rssidentify(arcl, config)
+    nxpix = rt.header(images, 'NAXIS1')
+        
+    # output wavelength calibrated, background substracted file
+    wavefits = infiles+'w.fits'
+    backfits = infiles+'ws.fits'
+    tmpfit = infiles+'sky.fits'
+    error_file = infiles+'_var.fits'
+    wavefits_err = infiles+'w_var.fits'
+    backfits_err = infiles+'ws_var.fits'
+
+    rt.loadparam(config, ['iraf.transform', 'iraf.fit1d'])
+    # check if the error file exist       
+    if do_error:
+        iraf.errorpars.errtype='uniform'
+        iraf.errorpars.resample='no'
+        iraf.samplepars.axis=1
+        iraf.samplepars.setParam('naverage', config['iraf.fit1d']['naverage'])
+        iraf.samplepars.setParam('low_reject', config['iraf.fit1d']['low_reject'])
+        iraf.samplepars.setParam('high_reject', config['iraf.fit1d']['high_reject'])
+        iraf.samplepars.setParam('niterate', config['iraf.fit1d']['niterate'])
+        iraf.samplepars.setParam('grow', config['iraf.fit1d']['grow'])
+               
+    rt.rmexist([wavefits, backfits, wavefits_err, backfits_err])
+    # Wavelength calibration	   
+    iraf.transform(infiles, wavefits, fitnames=ref_spec, logfiles = logfile)
+    iraf.fit1d(wavefits, tmpfit, "fit", axis=2)                    
+    iraf.imarith(wavefits, "-", tmpfit, backfits)
+
+    # Calibrated error file
+    if do_error:
+        iraf.transform(error_file, wavefits_err, fitnames=ref_spec, logfiles = logfile)
+        iraf.gfit1d(wavefits, 'tmptab.tab', function=config['iraf.fit1d']['function'], order=config['iraf.fit1d']['order'], xmin=1, xmax=nxpix, interactive='no')		       
+        iraf.tabpar('tmptab.tab',"rms", 1, Stdout=logfile)
+        rmsval = float(iraf.tabpar.value)
+        tmpval=rmsval*rmsval
+        print 'RMS of the fit (for VAR plane propagation) = %s ' %rmsval
+        iraf.imexpr("a+b",backfits_err, a=wavefits_err, b=tmpval)
+
+    print "**** Done with image %s ****" % (images)  
+
+
+# Flux calibration using input sensitivity curve    
+def rssfluxdefine(images,calflux, do_error=False):
+
+    print "We will use setups file: %s" %(calflux)
+    
+    obj_nofits = os.path.splitext(images)[0]+'ws'
+    inobj = obj_nofits+'.fits'
+    inobj_err = obj_nofits+'_var.fits'
+    calfits = obj_nofits+'f.fits'   
+    rt.rmexist([calfits])
+    
+    if not rt.header(inobj_err,'AIRMASS'): rt.rssairmass(inobj_err)
+
+    iraf.longslit.calibrate(input=inobj, output=calfits, extinct='yes', flux='yes',
+                            fnu='no',sensitivity=calflux, extinction=extinct)
+    rt.putheader(calfits, 'STDNAME', calflux)
+ 
+    if do_error: 
+        calfits_err = obj_nofits+'f_var.fits'
+        obj_err = obj_nofits+'f_err.fits'
+        rt.rmexist([calfits_err, obj_err])
+        # Convert variance to error
+        iraf.imexpr("sqrt(a)", obj_err, a = inobj_err)
+        iraf.longslit.calibrate(input=obj_err, output=calfits_err, extinct='yes',
+                                flux='yes',observa='saao',fnu='no', sensitivity=calflux,
+                                extinction=extinct)
+             
+# Estimate sensitivity curve using spectrophotometric standard
+def rssstdred(stdlist,stdarclist, sens, config, logfile):
+    
+    instdlist = read_fromfile(stdlist)
+    instdarclist  = read_fromfile(stdarclist)
+    stdarcfile=instdarclist[0]
+
+    inmake=str(instdlist[0])
+    stdsetup = rt.makesetups(stdarcfile,'std')
+    
+    rssdate = inmake.split('P')[1][0:8]
+    std_out = str(stdsetup)+"_"+rssdate
+    sens_out = std_out+'.fits'
+    std_fits= str(std_out)+".fits"
+    print "Here is the standard setup file : %s" %(std_fits)
+    rt.rmexist([std_out,std_fits])
+           
+    for instdl in instdlist: 
+
+        rssextractone(instdl,stdarcfile, config, logfile, do_error=False)        
+        obj_nofits = os.path.splitext(instdl)[0]+'ws'
+        inobj = obj_nofits+'.fits'
+        star_name= (rt.header(instdl,'OBJECT')).lower()
+        star_database = caldir+str(star_name)+".dat"
+        onedfits = obj_nofits+"1d.fits"
+        onedfits1 = obj_nofits+".0001.fits"
+        rt.rmexist([onedfits,onedfits1])
+
+        if not os.path.isfile(star_database):
+            print ""
+            print "*****  Cannot find file ",star_database, "*****"
+            print "*****  in the directory caldir *****"
+            print ""
+            return
+        rt.loadparam(config, ['iraf.apall'])
+        iraf.apall(inobj, b_niterate=5, b_sample='-100:-40,40:100', b_high_reject=1.5, b_low_reject=2)
+
+        os.system('mv %s %s' %  (onedfits1,onedfits))
+
+        if not rt.header(onedfits,'AIRMASS'): rt.rssairmass(onedfits)
+        print "**** start running standard %s" % (std_out)
+
+        iraf.standard(input=onedfits, output=std_out, caldir=caldir, interact='yes',
+                      star_name=star_name, extinct=extinct)
+        
+    # Sensitivity files    
+    iraf.sensfunc(std_out, sens_out, interactive= 'yes', extinct=extinct,
+                  newextinction='extinct.dat')
+    rt.getsh('cp %s %s' %  (sens_out,str(saltreddir))) 
+    rt.putheader(sens_out, 'FITSNAME', instdlist)
+
+    for instdl in instdlist:    
+        obj_nofits = os.path.splitext(instdl)[0]+'ws'
+        inobj = obj_nofits+'.fits'
+        in_cal = obj_nofits+'1d.fits'
+        out_cal = obj_nofits+'1dp.fits'
+        rt.rmexist([out_cal])
+        iraf.calibrate(in_cal, output=out_cal, sensitivity=std_fits, extinction=extinct)
+
+    return std_fits
+            
+        
+def read_fromfile(infile):
+    listout=[]
+    with open(infile) as FileObj:
+        listout = FileObj.read().splitlines()
+    return listout     
+
+       
+def reduce(objlist,objarclist,stdlist='', stdarclist='', logfile='saltlongslit.log',
+           parfile='/home/rajoelimananaa/software/saltpy/saltparam.txt', do_error=True):
+
+    inobjlist = read_fromfile(objlist)
+    inobjarclist = read_fromfile(objarclist)
+    arcfile=inobjarclist[0]
+
+    # get parameter files to dict    
+    config = rt.load_config('rssconfig.yml')
+    do_error = True
+    if not os.path.exists('database'): os.system('mkdir database')
+    if not os.path.isfile('login.cl'): os.system('mkiraf')
+
+    # check Flux calibration file
+    
+    if (stdlist != '' and os.path.splitext(stdlist)[0] == stdlist):
+        stdlist = rssstdred(stdlist, stdarclist,'yes', config, logfile)
+        print stdlist
+        if (os.path.splitext(stdlist)[0] != stdlist and os.path.isfile(stdlist)): 
+             print 'Good to go'
+        else:
+             return  
+    for inobj in inobjlist:
+             
+        print "Starting with object %s and %s" %(inobj,arcfile)
+        rssextractone(inobj, arcfile, config, logfile, do_error=do_error)
+        if (os.path.splitext(stdlist)[0] != stdlist and os.path.isfile(stdlist)):
+            print "***** Standard file found =  %s *****" %(stdlist)
+            rssfluxdefine(inobj, stdlist, do_error=do_error)
+
---
+++ rsstools.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python2
+#########################################################################################
+#                                                                                       #							
+#   Dr. Andry Rajoelimanana                                                             #	
+#                                                                                       #
+#   SALT/RSS longslit reduction                                                         #
+#                                                                                       #		
+#########################################################################################
+
+import yaml
+from astropy.io import fits 
+import os, sys
+from pyraf import iraf
+import numpy as np
+iraf.noao(_doprint=0)
+iraf.twodspec(_doprint=0)
+iraf.longslit(_doprint=0)
+iraf.apextract(dispaxis='1')
+
+def header(images, param):
+    hdrs = fits.getheader(str(images))    
+    if param in hdrs.keys():
+        return hdrs[param]
+    else:
+        return False
+        
+# Add headers        
+def putheader(images, param, value):    
+    iraf.hedit(images, fields=param, value=value, add='yes', addonly='yes',
+               verify='no',show='yes')        
+
+# Get shell output        
+def getsh(command):
+        child = os.popen(command)
+        data = child.read()
+        err = child.close()
+        if err:
+                raise RuntimeError, '%s failed with error code %d' \
+                        % (command, err)
+                print '\nTHIS IS VERY BAD!!!'
+                sys.exit(1)
+        return data
+
+# Make calibaration file using RSS setups 
+def makesetups(images, maketype):     
+       LAMPID = header(images,'LAMPID').replace(' ','')
+       GRATING = header(images,'GRATING')
+       GRANGLE = "%.2f" % header(images,'GR-ANGLE')
+       ARANGLE = "%.2f" % header(images,'AR-ANGLE')
+       BIN = header(images,'CCDSUM').replace(' ','x')     
+       if (maketype == 'std'):
+           sts = "Std_"+GRATING+"_"+ARANGLE+"_"+GRANGLE+"_"+BIN
+       else:
+           sts = LAMPID+"_"+GRATING+"_"+ARANGLE+"_"+GRANGLE+"_"+BIN
+       return sts
+
+
+# Remove existing file
+def rmexist(inlist):
+    for infile in inlist:
+        if os.path.isfile(infile):
+            os.remove(infile)
+
+# Merge configuration file
+def config_merge(custom, default):
+    if isinstance(custom, dict) and isinstance(default, dict):
+        for k,v in default.iteritems():
+            if k not in custom:
+                custom[k] = v
+            else:
+                custom[k] = config_merge(custom[k], v)
+    return custom
+
+# Load configuration file
+def loadparam(config, sections):
+    for section_name in sections:
+        params = config[section_name].items()
+        for param_id in params: 
+            eval(section_name).setParam(param_id[0], param_id[1])    
+
+# Load configuration file and merge it with the default one            
+def load_config(config_file):
+    config_default='/home/rajoelimananaa/software/saltpy/rssconf.yml'
+    config = yaml.safe_load(open(config_default))
+    custom_config = {}
+    if os.path.exists(config_file):
+        custom_config = yaml.safe_load(open(config_file))
+        
+    config = config_merge(custom_config, config)
+
+    sections = ['iraf.identify',
+            'iraf.reidentify',
+            'iraf.fitcoords',
+            'iraf.transform',
+            'iraf.fit1d',
+            'iraf.apall',
+            ]
+
+    for section_name in sections:
+        params = config[section_name].items()
+        for param_id in params:
+            eval(section_name).setParam(param_id[0], param_id[1])
+    return config    
+
+
+# Prepare images for reduction
+def prepare_image(images, do_error=False):
+
+    hdrs = fits.getheader(images)
+    if 'OBSERVAT' not in hdrs.keys(): putheader(images, 'OBSERVAT', 'SAAO')
+    if 'DISPAXIS' not in hdrs.keys(): putheader(images, 'DISPAXIS', '1')
+    if 'AIRMASS' not in hdrs.keys(): rssairmass(images)
+    
+    if do_error:
+        error_file = os.path.splitext(images)[0]+'_var.fits'
+        hdrerrs =  fits.getheader(error_file)
+        if 'OBSERVAT' not in hdrerrs.keys(): putheader(error_file, 'OBSERVAT', 'SAAO')
+        if 'DISPAXIS' not in hdrerrs.keys(): putheader(error_file, 'DISPAXIS', 1)
+        if 'AIRMASS' not in hdrerrs.keys(): rssairmass(error_file)
+
+
+# Calculate airmass
+def rssairmass(images):
+
+    airms = header(images, 'TELALT')
+    airmass = "%.5f" % (1./np.cos(np.radians(90.0-airms)))
+    putheader(images, 'AIRMASS', airmass)
+
+
+# Get approximate wavelength range
+def rsswave(images):
+
+    grating = header(images,'GRATING')[0:6]
+    grang = float(header(images,'GR-ANGLE'))
+    arang = float(header(images,'CAMANG'))
+    cbin = int(header(images,'CCDSUM').split(' ')[0])
+    cols = int(header(images,'NAXIS1'))
+    if (grating == 'PG0300'):
+        grat=300
+        gamma0 = 0.0
+    elif grating=='PG0900':
+        grat=903.2
+        gamma0 = -0.265
+    elif grating=='PG1300':
+        grat=1299.76
+        gamma0 = -0.265
+    elif grating=='PG1800':
+        grat=1801.89
+        gamma0 = -0.265
+    elif grating=='PG2300':
+        grat=2302.60
+        gamma0 = -0.265
+    elif grating=='PG3000':
+        grat=3000.55
+        gamma0 = -0.265
+    T2Con=-5.00	
+    T3Con=-1.00
+    FCampoly = np.asarray([-0.0023,0.0365,-0.2100,0.5061,-0.1861,328.697])
+    Grat0 = 1.407798403
+    ArtErr = -4.2E-05
+    Home0 = -0.063025809
+    alpha_r = np.radians(grang+Grat0)
+    beta0_r = np.radians(arang*(1+ArtErr)+Home0)-alpha_r
+    gam0_r = np.radians(gamma0)
+    lam0 = 1e7*np.cos(gam0_r)*(np.sin(alpha_r) + np.sin(beta0_r))/grat
+    ww = lam0/1000. - 4.
+    fcam = np.polyval(FCampoly,ww)
+    lmm = grat
+    disp = (1e7*np.cos(gam0_r)*np.cos(beta0_r)/lmm)/(fcam/.015)
+    dfcam = 3.162*disp*np.polyval([FCampoly[x]*(5-x) for x in range(5)],ww)
+    T2 = -0.25*(1e7*np.cos(gam0_r)*np.sin(beta0_r)/lmm)/(fcam/47.43)**2 + T2Con*disp*dfcam
+    T3 = (-1./24.)*3162.*disp/(fcam/47.43)**2 + T3Con*disp
+    T0 = lam0 + T2 
+    T1 = 3162.*disp + 3*T3
+    X = (np.array(range(cols))+1-cols/2)*cbin/3162.
+    lam_X = T0+T1*X+T2*(2*X**2-1)+T3*(4*X**3-3*X)
+    iraf.hedit(images, "CRVAL1", min(lam_X), add='yes', addonly='yes', verify='no' )
+    iraf.hedit(images, "CRPIX1", 1., add='yes', addonly='yes', verify='no' )
+    iraf.hedit(images, "CDELT1", (max(lam_X)-min(lam_X))/cols, add='yes', addonly='yes', verify='no' )
+    iraf.hedit(images, "CD1_1", (max(lam_X)-min(lam_X))/cols, add='yes', addonly='yes', verify='no' )
+    
+    return
+ 
