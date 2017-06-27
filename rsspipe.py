7e8db245748eb0602b1ae06ef901af1677f358dc
AndryRajoelimanana <randryfitiavana@gmail.com>
2017-06-27 12:32:38 +0200

    Update rsspipe.py

--- rsspipe.py
+++ rsspipe.py
@@ -22,14 +22,10 @@ iraf.stsdas(_doprint=0)
 iraf.analysis(_doprint=0)
 iraf.fitting(_doprint=0)
 
-config = rt.load_config('rssconfig.yml')
-
-saltreddir = config['rss']['rssdir'] 
-caldir = config['rss']['caldir']
-extinct= config['rss']['extdir']
-
 # Wavelength Calibration of the ARC spectra       
 def rssidentify(arclist, config):
+
+    saltreddir = config['rss']['rssdir'] 
     ref_spec= os.path.splitext(arclist)[0]
     reference= rt.makesetups(arclist,"arc")
     referfits = reference+'.fits'  
@@ -44,6 +40,13 @@ def rssidentify(arclist, config):
         print "***** rssidentify error"
         print "***** We can not get your spectral setup *****"
         return
+
+
+    if os.path.isfile(coordfile):
+        rt.getsh('cp %s ./' %  (coordfile))
+    else :
+        print "***** rssidentify error *** The coordinate list for this arc IS NOT in our directory *****"
+        return            
     
     if os.path.isfile(referfits_db) and os.path.isfile(referid_db):
         rt.getsh('cp %s ./'  % (referfits_db))
@@ -55,11 +58,6 @@ def rssidentify(arclist, config):
         print "This setup is not in our database (setup = %s)" % (reference)
         rt.getsh('cp %s ./%s'  % (arclist, referfits))
         rt.rsswave(referfits)
-        if os.path.isfile(coordfile):
-            rt.getsh('cp %s ./' %  (coordfile))
-        else :
-            print "***** rssidentify error *** The coordinate list for this arc IS NOT in our directory *****"
-            return            
         iraf.identify(reference, coordlist=coordlist, mode='h')
         iraf.reidentify(reference, reference, coordlist=coordlist, mode='h')
         iraf.fitcoord(reference, mode='h')
@@ -85,10 +83,14 @@ def rssextractone(images, arcl, config, logfile, do_error=False):
     print "'***** Reducing of objects ***** '"
     infiles = os.path.splitext(images)[0]
     ref_spec = os.path.splitext(arcl)[0]
-    if os.path.isfile('b'+images): os.system('mv b'+images+' '+images)
-    hduorig= fits.open(images)
+    if os.path.isfile('b'+images): 
+        hduorig= fits.open('b'+images)
+        os.remove(images)
+    else:
+        hduorig= fits.open(images)
+        os.system('mv '+images+'  b'+images)
+
     hduimg = fits.PrimaryHDU(data=hduorig[0].data,header=hduorig[0].header)
-    os.system('mv  '+images+'  b'+images)
     hduimg.writeto(images)
     hduvarimg = fits.PrimaryHDU(data=hduorig[1].data,header=hduorig[0].header)
     rt.rmexist([infiles+'_var.fits'])
@@ -137,13 +139,14 @@ def rssextractone(images, arcl, config, logfile, do_error=False):
 
 
 # Flux calibration using input sensitivity curve    
-def rssfluxdefine(images,calflux, do_error=False):
+def rssfluxdefine(images,calflux,config, do_error=False):
 
     print "We will use setups file: %s" %(calflux)
-    
+     
     obj_nofits = os.path.splitext(images)[0]+'ws'
     inobj = obj_nofits+'.fits'
     inobj_err = obj_nofits+'_var.fits'
+    extinct = config['rss']['extdir']
     calfits = obj_nofits+'f.fits'   
     rt.rmexist([calfits])
     
@@ -166,9 +169,13 @@ def rssfluxdefine(images,calflux, do_error=False):
 # Estimate sensitivity curve using spectrophotometric standard
 def rssstdred(stdlist,stdarclist, sens, config, logfile):
     
-    instdlist = read_fromfile(stdlist)
-    instdarclist  = read_fromfile(stdarclist)
+    instdlist = np.loadtxt(stdlist, unpack=True, ndmin=1, dtype=str)
+    instdarclist = np.loadtxt(stdarclist, unpack=True, ndmin=1, dtype=str)
     stdarcfile=instdarclist[0]
+    saltreddir = config['rss']['rssdir'] 
+    caldir = config['rss']['caldir']
+    extinct= config['rss']['extdir']
+
 
     inmake=str(instdlist[0])
     stdsetup = rt.makesetups(stdarcfile,'std')
@@ -232,16 +239,15 @@ def read_fromfile(infile):
     return listout     
 
        
-def reduce(objlist,objarclist,stdlist='', stdarclist='', logfile='saltlongslit.log',
-           parfile='/home/rajoelimananaa/software/saltpy/saltparam.txt', do_error=True):
+def reduce(objlist,objarclist,stdlist='', stdarclist='', logfile='saltlongslit.log', do_error=True):
 
-    inobjlist = read_fromfile(objlist)
-    inobjarclist = read_fromfile(objarclist)
+    inobjlist = np.loadtxt(objlist, unpack=True, ndmin=1, dtype=str)
+    inobjarclist = np.loadtxt(objarclist, unpack=True, ndmin=1, dtype=str)
     arcfile=inobjarclist[0]
 
     # get parameter files to dict    
     config = rt.load_config('rssconfig.yml')
-    do_error = True
+
     if not os.path.exists('database'): os.system('mkdir database')
     if not os.path.isfile('login.cl'): os.system('mkiraf')
 
@@ -260,5 +266,8 @@ def reduce(objlist,objarclist,stdlist='', stdarclist='', logfile='saltlongslit.l
         rssextractone(inobj, arcfile, config, logfile, do_error=do_error)
         if (os.path.splitext(stdlist)[0] != stdlist and os.path.isfile(stdlist)):
             print "***** Standard file found =  %s *****" %(stdlist)
-            rssfluxdefine(inobj, stdlist, do_error=do_error)
+            rssfluxdefine(inobj, stdlist, config, do_error=do_error)
+            
+            
+            
 
